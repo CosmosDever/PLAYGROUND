371ba183f05fb8e65c24616a271507c6e5068a7c
CosmosDever <46952477+CosmosDever@users.noreply.github.com>
2022-10-21 11:49:47 +0700

    Update tspy.py

--- tspy.py
+++ tspy.py
@@ -1,2 +1,7 @@
-l=input().split('')
-print(l)
+atext='The split method is used to split the strings and store them in the list. The built-in method returns a list of the words in the string, using the “delimiter” as the delimiter string. If a delimiter is not specified or is None, a different splitting algorithm is applied: runs of consecutive whitespace are regarded as a single separator, and the result will contain no empty strings at the start or end if the string has leading or trailing whitespace.'    
+ans=''
+ltext = atext.lower().split(" ")
+def replace_the_map(text):
+    rtext = list(map(lambda i : 'xxx' if i =='the' else i,ltext))        
+    return ' '.join(rtext)
+print(replace_the_map(atext))   
